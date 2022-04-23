b8e945cfb2a10149de2d9d3074eb0d5fec288b5f
Jerrycda <jcda1031@hotmail.com>
2022-04-23 15:34:51 -0500

    calculadora de porcentajes

--- descuento.js
+++ descuento.js
@@ -11,15 +11,16 @@ function calcularPrecioConDescuento(precio, descuento){
 
 function onClickButtonPriceDiscount(){
     const inputPrice = document.getElementById("InputPrice");
-    const priceValue = Number(inputPrice.value);
+    const priceValue = inputPrice.value;
 
     const inputDiscount = document.getElementById("InputDiscount");
-    const discountValue = Number(inputDiscount.value); 
+    const discountValue = inputDiscount.value; 
+
 
     const precioConDescuento = calcularPrecioConDescuento(priceValue, discountValue);
-
     const resultP = document.getElementById("ResultP");
     resultP.innerText = "El precio con descuento son: $" + precioConDescuento;
+   
 }
 
 //console.log({
